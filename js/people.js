b913d5a3f9b59e95221be13957b15c53fde7ea9b
fedesilva <fede.silva@gmail.com>
2014-04-03 19:04:49 -0300

    _.walk samples

---
+++ js/people.js
@@ -0,0 +1,16 @@
+/**
+ * Created by f on 4/3/14.
+ */
+
+"use strict";
+
+var people = [
+  { name: "federico silva",           age: 41, gender: "male",    job: { at: "Batanga",          position: "Architect", salary: 1}      },
+  { name: "Mark Shuttleworth",        age: 41, gender: "male",    job: { at: "Ubuntu",           position: "SABDFL",    salary: 0}      },
+  { name: "Marissa Mayer",            age: 38, gender: "female",  job: { at: "YAHOO",            position: "CEO",       salary: 1000}   },
+  { name: "Martin Odersky",           age: 55, gender: "male",    job: { at: "EPFL",             position: "Professor", salary: 120}    },
+  { name: "Linus Benedict Torvalds",  age: 44, gender: "male",    job: { at: "Linux Foundation", position: "Badass",    salary: 250}    },
+  { name: "Larry Page",               age: 44, gender: "male",    job: { at: "Google",           position: "CEO",       salary: 6500}   }
+];
+
+
